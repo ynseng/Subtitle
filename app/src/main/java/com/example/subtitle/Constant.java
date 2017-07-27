cb5572747a66ff0ff9536bba8b81c27f357a3b69
johnny <121151270@qq.com>
2017-07-27 21:19:54 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/subtitle/Constant.java
@@ -0,0 +1,9 @@
+package com.example.subtitle;
+
+public class Constant {
+    //可以替换为网络视频地址
+	public static String videoUrl1 = "/storage/emulated/0/privatefile/testhw.mp4" ;
+//	public static String videoUrl1 = Environment.getExternalStorageDirectory().getPath()+"/f2.mp4" ;
+	public static String srtUrl1 = "/storage/emulated/0/privatefile/testhw.srt";
+//	public static String srtUrl1 = Environment.getExternalStorageDirectory().getPath()+"/f2.srt" ;
+}
